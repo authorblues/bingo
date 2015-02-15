e98ec2a93d8f6b20e5bfe81a26ba0e4985131615
Chad Waters <authorblues@gmail.com>
2015-02-15 00:56:07 -0500

    Initial commit for LOZ bingo

---
+++ games/loz.js
@@ -0,0 +1,97 @@
+{
+	"name": "The Legend of Zelda",
+	"platform": "NES",
+	
+	"goals": [
+		{ "name": "Beat Level 1 (Triforce)", "difficulty":  3, "tags": ["L1"] },
+		{ "name": "Beat Level 2 (Triforce)", "difficulty":  3, "tags": ["L2"] },
+		{ "name": "Beat Level 3 (Triforce)", "difficulty":  4, "tags": ["L3"] },
+		{ "name": "Beat Level 4 (Triforce)", "difficulty":  5, "tags": ["L4"] },
+		{ "name": "Beat Level 5 (Triforce)", "difficulty":  8, "tags": ["L5"] },
+		{ "name": "Beat Level 6 (Triforce)", "difficulty": 12, "tags": ["L6"] },
+		{ "name": "Beat Level 7 (Triforce)", "difficulty":  9, "tags": ["L7"] },
+		{ "name": "Beat Level 8 (Triforce)", "difficulty": 12, "tags": ["L8"] },
+		
+		{ "name": "Find Level 9", "difficulty": 2, "tags": ["L9"] },
+		
+		{ "name": "Pay for Door Repairs x3", "difficulty": 4 },
+		
+		{ "name": "3 Dungeon Maps", "difficulty": 3, "tags": ["maps"] },
+		{ "name": "5 Dungeon Maps", "difficulty": 5, "tags": ["maps"] },
+		
+		{ "name": "3 Compasses", "difficulty": 3, "tags": ["compass"] },
+		{ "name": "5 Compasses", "difficulty": 5, "tags": ["compass"] },
+		
+		{ "name": "Fully Explore Level 1", "difficulty":  4, "tags": ["L1"] },
+		{ "name": "Fully Explore Level 2", "difficulty":  4, "tags": ["L2"] },
+		{ "name": "Fully Explore Level 3", "difficulty":  5, "tags": ["L3"] },
+		{ "name": "Fully Explore Level 4", "difficulty":  6, "tags": ["L4"] },
+		{ "name": "Fully Explore Level 5", "difficulty": 10, "tags": ["L5"] },
+		{ "name": "Fully Explore Level 6", "difficulty": 14, "tags": ["L6"] },
+		{ "name": "Fully Explore Level 7", "difficulty": 11, "tags": ["L7"] },
+		{ "name": "Fully Explore Level 8", "difficulty": 14, "tags": ["L8"] },
+		
+		{ "name": "10+ Heart Containers", "difficulty":  9, "tags": ["swordhearts"] },
+		{ "name": "12+ Heart Containers", "difficulty": 12, "tags": ["swordhearts"] },
+		{ "name": "14+ Heart Containers", "difficulty": 16, "tags": ["swordhearts"] },
+		
+		{ "name": "3 or Fewer Heart Containers", "difficulty": 18, "tags": ["swordhearts"] },
+		{ "name": "5 or Fewer Heart Containers", "difficulty": 15, "tags": ["swordhearts"] },
+		{ "name": "7 or Fewer Heart Containers", "difficulty":  7, "tags": ["swordhearts"] },
+		
+		{ "name": "Finish with 255 Rupees", "difficulty": 5, "tags": ["rupees"] },
+		
+		{ "name": "Give Letter to Old Lady", "difficulty": 3 },
+		
+		{ "name": "Swordless", "difficulty": 20, "tags": ["sword", "-ganon"] },
+		{ "name": "Wooden Sword Only", "difficulty": 16, "tags": ["sword"] },
+		{ "name": "White Sword", "difficulty": 4, "tags": ["sword"] },
+		{ "name": "Magical Sword", "difficulty": 14, "tags": ["sword"] },
+		
+		{ "name": "Finish with 6+ Keys",  "difficulty":  6, "tags": ["keys"] },
+		{ "name": "Finish with 9+ Keys",  "difficulty": 10, "tags": ["keys"] },
+		{ "name": "Finish with 12+ Keys", "difficulty": 15, "tags": ["keys"] },
+		
+		{ "name": "Beat Ganon", "difficulty": 20, "tags": ["ganon"] },
+		
+		{ "name": "Acquire Life Potion", "difficulty": 3 },
+		{ "name": "Acquire Bow", "difficulty": 3, "tags": ["bow"] },
+		{ "name": "Acquire Wooden Arrows (No Silver)", "difficulty": 3, "tags": ["bow", "-ganon"] },
+		{ "name": "Acquire Silver Arrow", "difficulty": 5, "tags": ["bow", "ganon"] },
+		{ "name": "Acquire Boomerang", "difficulty": 5, "tags": ["boomerang"] },
+		{ "name": "Acquire Magical Boomerang", "difficulty": 5, "tags": ["boomerang"] },
+		{ "name": "Acquire Blue Candle (No Red)", "difficulty": 3, "tags": ["candle"] },
+		{ "name": "Acquire Red Candle", "difficulty": 5, "tags": ["candle"] },
+		{ "name": "Acquire Recorder", "difficulty": 4 },
+		{ "name": "Finish with Bait", "difficulty": 3 },
+		{ "name": "Acquire Wand", "difficulty": 5 },
+		{ "name": "Acquire Raft", "difficulty": 4 },
+		{ "name": "Acquire Book", "difficulty": 5 },
+		{ "name": "Acquire Ladder", "difficulty": 3 },
+		{ "name": "Acquire Magical Key", "difficulty": 5 },
+		{ "name": "Acquire Power Bracelet", "difficulty": 1 },
+		{ "name": "Acquire Red Ring (No Blue)", "difficulty": 5 },
+		{ "name": "Acquire Blue Ring", "difficulty": 6, "tags": ["rupees"] },
+		
+		{ "name": "Defeat Aquamentis (Level 1)", "difficulty": , "tags": ["L1"] },
+		{ "name": "Defeat Dodongo (Level 2)", "difficulty": , "tags": ["L2", "bombs"] },
+		{ "name": "Defeat Manhandla (Level 3)", "difficulty": , "tags": ["L3"] },
+		{ "name": "Defeat Gleeok (Level 4)", "difficulty": , "tags": ["L4"] },
+		{ "name": "Defeat Digdogger (Level 5)", "difficulty": , "tags": ["L5"] },
+		{ "name": "Defeat Gohma (Level 6)", "difficulty": , "tags": ["L6"] },
+		{ "name": "Defeat Aquamentis (Level 7)", "difficulty": , "tags": ["L7"] },
+		{ "name": "Defeat Gleeok (Level 8)", "difficulty": , "tags": ["L8"] },
+	],
+	
+	"tags": {
+		"L1": { "allowmultiple": false },
+		"L2": { "allowmultiple": false },
+		"L3": { "allowmultiple": false },
+		"L4": { "allowmultiple": false },
+		"L5": { "allowmultiple": false },
+		"L6": { "allowmultiple": false },
+		"L7": { "allowmultiple": false },
+		"L8": { "allowmultiple": false },
+		"L9": { "allowmultiple": false },
+	},
+}
