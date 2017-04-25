0a252427d0779a2d791bf5f4a254ffc1714e7718
Forstride <forstride@gmail.com>
2017-04-24 21:46:56 -0400

    Create yl.js

---
+++ games/yl.js
@@ -0,0 +1,88 @@
+{
+	"name": "Yooka-Laylee",
+	"author": "Forstride",
+	"platform": "PC/PS4/Xbox One/Switch",
+	
+	"rules": [
+		"Timer starts when pressing A to start a new file.",
+		"Timer ends when you complete a row/column/diagonal.",
+		"Co-op is not allowed."],
+	
+	"goals": [
+		{ "name": "Collect Clara's Pagie in Tribalstack Tropics", "difficulty": 1, "tags": ["Pagies"] },
+		{ "name": "Collect Duke's Pagie in Tribalstack Tropics", "difficulty": 1, "tags": ["Pagies"] },
+		{ "name": "Collect the Knights of Hamalot in Tribalstack Tropics", "difficulty": 2, "tags": ["Pagies"] },
+		{ "name": "Collect Duke's Pagie in Tribalstack Tropics", "difficulty": 1, "tags": ["Pagies"] },
+		
+		{ "name": "Collect the Tribalstack Tropics Health Extender", "difficulty": 1, "tags": ["Health Extenders"] },
+		{ "name": "Collect the Glitterglaze Glacier Health Extender", "difficulty": 2, "tags": ["Health Extenders"] },
+		{ "name": "Collect the Moodymaze Marsh Health Extender", "difficulty": 3, "tags": ["Health Extenders"] },
+		{ "name": "Collect the Capital Cashino Health Extender", "difficulty": 4, "tags": ["Health Extenders"] },
+		{ "name": "Collect the Galleon Galaxy Health Extender", "difficulty": 5, "tags": ["Health Extenders"] },
+		{ "name": "Collect the Hivory Towers Health Extender", "difficulty": 1, "tags": ["Health Extenders"] },
+		
+		{ "name": "Collect the Tribalstack Tropics Power Extender", "difficulty": 1, "tags": ["Power Extenders"] },
+		{ "name": "Collect the Glitterglaze Glacier Power Extender", "difficulty": 2, "tags": ["Power Extenders"] },
+		{ "name": "Collect the Moodymaze Marsh Power Extender", "difficulty": 3, "tags": ["Power Extenders"] },
+		{ "name": "Collect the Capital Cashino Power Extender", "difficulty": 4, "tags": ["Power Extenders"] },
+		{ "name": "Collect the Galleon Galaxy Power Extender", "difficulty": 5, "tags": ["Power Extenders"] },
+		{ "name": "Collect the Hivory Towers Power Extender", "difficulty": 1, "tags": ["Power Extenders"] },
+		
+		{ "name": "Learn Sonar Shot", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Slurp Shot", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Buddy Slam", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Slurp State", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Lizard Leap", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Sonar 'Splosion", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Lizard Lash", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Reptile Rush", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		{ "name": "Learn Sonar Shield", "difficulty": 1, "tags": ["Trowzer Moves"] },
+		
+		{ "name": "Complete Kartos Karting", "difficulty": 1, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Glaciators", "difficulty": 2, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Bee Bop", "difficulty": 3, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Hurdle Hijinx", "difficulty": 4, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Up 'N' Nova", "difficulty": 5, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Gun-tlet Run", "difficulty": 1, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Blag the Flag", "difficulty": 1, "tags": ["Rextro Minigames"] },
+		{ "name": "Complete Jobstacle Course", "difficulty": 1, "tags": ["Rextro Minigames"] },
+		
+		{ "name": "Complete the Tribalstack Tropics Kartos track", "difficulty": 1, "tags": ["Kartos Tracks"] },
+		{ "name": "Complete the Glitterglaze Glacier Kartos track", "difficulty": 2, "tags": ["Kartos Tracks"] },
+		{ "name": "Complete the Moodymaze Marsh Kartos track", "difficulty": 3, "tags": ["Kartos Tracks"] },
+		{ "name": "Complete the Capital Cashino Kartos track", "difficulty": 4, "tags": ["Kartos Tracks"] },
+		{ "name": "Complete the Galleon Galaxy Kartos track", "difficulty": 5, "tags": ["Kartos Tracks"] },
+		
+		{ "name": "Defeat The Great Rampo", "difficulty": 1, "tags": ["Bosses"] },
+		{ "name": "Defeat Brrreeze Blok", "difficulty": 1, "tags": ["Bosses"] },
+		{ "name": "Defeat Trev the Tenteyecle", "difficulty": 1, "tags": ["Bosses"] },
+		{ "name": "Defeat I.N.E.P.T.", "difficulty": 1, "tags": ["Bosses"] },
+		{ "name": "Defeat Planette", "difficulty": 1, "tags": ["Bosses"] },
+		{ "name": "Defeat Capital B", "difficulty": 1, "tags": ["Bosses"] }
+
+	],
+	
+	"difficulty": {
+		"easymax": 3,
+		"normmin": 3,
+		"normmax": 6,
+		"hardmin": 5
+	},
+	
+	"tags": {
+		"Pagies": { "allowmultiple": true, "image": "http://i.imgur.com/CbEp2qu.png" },
+		"Health Extenders": { "allowmultiple": false, "image": "http://i.imgur.com/RotvIaA.png" },
+		"Power Extenders": { "allowmultiple": false, "image": "http://i.imgur.com/7yoKtBJ.png" },
+		"Trowzer Moves": { "allowmultiple": false, "image": "http://i.imgur.com/uGz6jpK.png" },
+		"Rextro Minigames": { "allowmultiple": false, "image": "http://i.imgur.com/wICXqvP.png" },
+		"Kartos Tracks": { "allowmultiple": false, "image": "http://i.imgur.com/31cbXMV.png" },
+		"Bosses": { "allowmultiple": false, "image": "http://i.imgur.com/raIkhkd.png" },
+		"Random Goals": { "allowmultiple": true, "image": "http://i.imgur.com/VsrwkZZ.png" }
+	},
+	
+	"modifiers": {
+		"Pagies": [
+			{ "name": "No Flappy Flight", "difficulty": 4 }
+		]
+	}
+}
